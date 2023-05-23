1a47349efb216c639c579e9d076c82e4def10cb8
[Bastien] <[bastien.pelletier@le-campus-numerique.fr]>
2023-05-23 14:28:29 +0200

    exo 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,10 @@
 // you can write js here
-console.log('hello from file');
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?") 
+// prompt permet d'interagir avec la console, me pose la question sur la page html
+ 
+let celsius = kelvins - 273;
+
+let fahrenheits = celsius * (9/5) + 32;
+
+console.log(kelvins);
+console.log(Math.floor(fahrenheits)); // j'affiche l'arrondi des fahrenheits
